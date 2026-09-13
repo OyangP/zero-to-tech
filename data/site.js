a4c6280880c53ed04ffab9cab58a4f5d7ea1a0bd
OyangP <oyp819@gmail.com>
2026-09-13 18:05:21 +0800

    项目从react升级到next.js框架

--- data/site.js
+++ data/site.js
@@ -10,7 +10,7 @@ export const home = {
   heroSubtitle: "项目，创意，灵感，心得，我的作品",
   featuredWork: {
     kicker: "作品",
-    title: "hello,文字实验室",
+    title: "文字实验室",
     copy: "拼音和情绪，挖掘中文里的细节",
     linkLabel: "打开作品",
   },
@@ -21,6 +21,6 @@ export const home = {
 };
 
 export const textLab = {
-  heroTitle: "Hello，文字实验室",
+  heroTitle: "文字实验室",
   heroSubtitle: "拼音和情绪，挖掘中文里的细节",
 };
